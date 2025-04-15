69e1cc064ced9416bb55a046fc4524f0b15caef2
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2025-04-15 16:40:05 -0400

    update logout return url

--- frontend/src/components/Auth/LogoutButton.js
+++ frontend/src/components/Auth/LogoutButton.js
@@ -4,10 +4,12 @@ import { useAuth0 } from '@auth0/auth0-react';
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
+  const returnToUrl = `${window.location.origin}${process.env.PUBLIC_URL || ""}/`; // Add trailing slash for consistency
+
   return (
     <button
       onClick={() =>
-        logout({ logoutParams: { returnTo: window.location.origin } })
+        logout({ logoutParams: { returnTo: returnToUrl } }) // Use the constructed URL
       }
     >
       Log Out
